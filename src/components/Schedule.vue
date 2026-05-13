ea5149abf568716d343a9310effb6f3dd8d8cc38
SamWen0201 <jqwertyu899800@gmail.com>
2026-05-13 21:31:13 +0800

    feat: finished part of Schedule overview logic, add el-button to Schedule component

--- src/components/Schedule.vue
+++ src/components/Schedule.vue
@@ -3,67 +3,121 @@ import { computed, onMounted, ref } from "vue";
 import { useRoomList } from "@/stores/roomList";
 import { useBookingList } from "@/stores/bookingListStore";
 import type { RoomTimeLine } from "@/types";
+import type { Booking } from "@/types";
 const useRoomListStore = useRoomList();
 const useBookingListStore = useBookingList();
 
 const today = ref("");
 
 // 先計算一天 9:00-18:00 之間可以分為多少 30分鐘
-function calculateTimeBlock([startTime, endTime]: string[], blockTime: number = 30): number {
-    const [startTimeHour, startTimeMinute] = startTime?.split(":").map(Number) as [number, number];
-    const [endTimeHour, endTimeMinute] = endTime?.split(":").map(Number) as [number, number];
+function calculateTimeBlock(
+  [startTime, endTime]: string[],
+  blockTime: number = 30,
+): number {
+  const [startTimeHour, startTimeMinute] = startTime
+    ?.split(":")
+    .map(Number) as [number, number];
+  const [endTimeHour, endTimeMinute] = endTime?.split(":").map(Number) as [
+    number,
+    number,
+  ];
 
-    return ((endTimeHour * 60 + endTimeMinute) - (startTimeHour * 60 + startTimeMinute)) / blockTime;
+  return (
+    (endTimeHour * 60 +
+      endTimeMinute -
+      (startTimeHour * 60 + startTimeMinute)) /
+    blockTime
+  );
 }
 
 const roomTimeLines = ref<RoomTimeLine[]>([]);
 
 onMounted(() => {
-    // 初始化 roomTimeLines state
-    roomTimeLines.value = useRoomListStore.rooms.map( (el) => {
-        const roomIsUsingArrLength = calculateTimeBlock(["9:00" , "18:00"]); 
-        const roomIsUsingArr: boolean[] =  new Array(roomIsUsingArrLength).fill(false);
+  // 初始化 roomTimeLines state
+  roomTimeLines.value = useRoomListStore.rooms.map((el) => {
+    const roomIsUsingArrLength = calculateTimeBlock(["9:00", "18:00"]);
+    const roomIsUsingArr: boolean[] = new Array(roomIsUsingArrLength).fill(
+      false,
+    );
 
-        return {
-            roomId: el.id,
-            roomIsUsingArr
-        }
-    })
-    
-})
+    return {
+      roomId: el.id,
+      roomIsUsingArr,
+    };
+  });
+});
 
 // 根據 Schedule 的 today state 來決定 roomIsUsingArr  各元素的 truthy value
 // 1. 先獲得今天的 預約紀錄 -> 今天 + 1 天，怎麼去找到屬於這一天的紀錄
-const todayBooking = computed(() => {
-    // return useBookingListStore.bookings.filter( (el) => )
-})
+//
+const todayBookings = computed<Booking[]>(() => {
+  // 將預約紀錄的日期等於今天日期的所有預約紀錄計算出來
+  return useBookingListStore.bookings.filter((el) => {
+    const bookingDate = new Date(el.startTime);
+    const todayDate = new Date(today.value);
 
+    const bookingYear = bookingDate.getFullYear();
+    const bookingMonth = bookingDate.getMonth();
+    const bookingDay = bookingDate.getDate();
 
+    const todayYear = todayDate.getFullYear();
+    const todayMonth = todayDate.getMonth();
+    const todayDay = todayDate.getDate();
 
+    return bookingYear === todayYear &&
+      bookingMonth === todayMonth &&
+      bookingDay === todayDay
+      ? true
+      : false;
+  });
+});
 
+// booking 中的 startTime and endTime 要能夠讓 roomTimeLines 中的屬性 roomIsUsingArr 中對應的陣列設定 truthy value
+// 假設 9:00 - 9:30 代表 roomIsUsingArr[0] = truth
+// (bookingStart - todayDate)= 開始時間多出來的 30n 分鐘 n 就會是 roomIsUsingArr 的索引值為 true 的元素？
+// (bookingEnd - bookingStart) / blockTime = 維持的格子數，也就是 由前面一個算式中的 n + 這邊算出來的數值？
+// 最後計算出來的元素是第 n ~ n+
+
+// calculateBlockSpan 這個 function 會在 todayBookings 中的 每一個 booking 跑一次
+// 造成的 side effect 是修改我們的 roomIsUsingArr 為對應到 startTime, endTime 的 truthy value
+function calculateBlockSpan(
+  bookingStartTime: number,
+  bookingEndTime: number,
+): void {}
 </script>
 <template>
-    <div class="schedule">
-        <h2>Schedule</h2>
-        <div class="block">
-          <el-date-picker
-            v-model="today"
-            type="date"
-            placeholder="Pick a day"
-            size="default"
-            @change=""
-            :default-time="new Date(Date.now())"
-          />
-        </div>
-        <div class="timeline-chart">
-            <ul>
-                <li v-for="room in useRoomListStore.rooms">
-                    <h3>{{ room.name }}</h3>
-                    <ul>
-                        <li></li>
-                    </ul>
-                </li>
-            </ul>
-        </div>
+  <div class="schedule">
+    <h2>Schedule</h2>
+    <div class="block">
+      <el-date-picker
+        v-model="today"
+        type="date"
+        placeholder="Pick a day"
+        size="default"
+        @change=""
+        :default-time="new Date(Date.now())"
+      />
+      <el-button type="primary">新增預約</el-button>
     </div>
+    <div class="timeline-chart">
+      <ul>
+        <li v-for="room in useRoomListStore.rooms">
+          <h3>{{ room.name }}</h3>
+          <ul>
+            <li></li>
+          </ul>
+        </li>
+      </ul>
+    </div>
+    <div>
+      <ul>
+        <li v-for="booking in todayBookings">
+          <span
+            >測試：今天的預約紀錄：{{ booking.title }}
+            {{ booking.userId }}</span
+          >
+        </li>
+      </ul>
+    </div>
+  </div>
 </template>
--- src/mock/index.ts
+++ src/mock/index.ts
@@ -1,5 +1,5 @@
-import type { Booking, Room, User } from "@/types"
-import  Mock  from "mockjs"
+import type { Booking, Room, User } from "@/types";
+import Mock from "mockjs";
 
 // mock data
 const rooms: Room[] = [
@@ -7,63 +7,86 @@ const rooms: Room[] = [
     id: "1",
     name: "會議室A",
     capacity: 6,
-    equipments: ['白板', '投影機']
+    equipments: ["白板", "投影機"],
   },
   {
     id: "2",
     name: "大會議室",
     capacity: 10,
-    equipments: ['投影機']
+    equipments: ["投影機"],
   },
   {
     id: "3",
     name: "中型會議室A",
     capacity: 8,
-    equipments: ['雷射筆']
+    equipments: ["雷射筆"],
   },
-]
+];
 
 const bookings: Booking[] = [
   {
-    id: '1',
-    roomId: '1', // 關聯的會議室 id
-    userId: '1', // 借閱人 id
-    title: '晨間會議', // 會議主題
+    id: "1",
+    roomId: "1", // 關聯的會議室 id
+    userId: "1", // 借閱人 id
+    title: "晨間會議", // 會議主題
     startTime: 1800406800000, // 2027, 1, 20, 9
     endTime: 1800415800000, // 2027, 1, 20, 11, 30
   },
   {
-    id: '2',
-    roomId: '3', // 關聯的會議室 id
-    userId: '2', // 借閱人 id
-    title: '全員大會', // 會議主題
+    id: "2",
+    roomId: "3", // 關聯的會議室 id
+    userId: "2", // 借閱人 id
+    title: "全員大會", // 會議主題
     startTime: 1801364400000, // 2027, 12, 31, 11
     endTime: 1801368000000, // 2027, 12, 31, 12
-  }
-]
+  },
+  {
+    id: "3",
+    roomId: "2",
+    userId: "1",
+    title: "臨時動議A",
+    startTime: 1778652000000,
+    endTime: 1778657400000,
+  },
+  {
+    id: "4",
+    roomId: "1",
+    userId: "1",
+    title: "臨時動議B",
+    startTime: 1778652000000,
+    endTime: 1778657400000,
+  },
+  {
+    id: "5",
+    roomId: "3",
+    userId: "1",
+    title: "臨時動議C",
+    startTime: 1778652000000,
+    endTime: 1778657400000,
+  },
+];
 
 const users: User[] = [
   {
-    id: '1',
-    role: 'admin',
-    name: 'SamWen'
+    id: "1",
+    role: "admin",
+    name: "SamWen",
   },
   {
-    id: '2',
-    role: 'normal',
-    name: "JohnW"
-  }
-]
-
+    id: "2",
+    role: "normal",
+    name: "JohnW",
+  },
+];
 
 // function
 
 // rooms api
-Mock.mock('/api/v1/rooms', 'get', () => {
+Mock.mock("/api/v1/rooms", "get", () => {
   return rooms;
-})
+});
 
 // bookings api
-Mock.mock('/api/v1/bookings', 'get', () => {
+Mock.mock("/api/v1/bookings", "get", () => {
   return bookings;
-})
+});
